dd03cbb4e0ea281894de981d723e00ea65c5e7a6
Piorosen <aoikazto@gmail.com>
2022-10-21 01:40:56 +0900

    빌드 명령어와 환경설정 코드 작성함.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 mkdir components
-docker run -d -p 8000:22 -v $(pwd):/workspaces -e PATH=/workspaces/components/cmake-3.23.4-linux-x86_64/bin:/workspaces/components/gcc-linaro-6.3.1-2017.02-x86_64_aarch64-linux-gnu/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin --name i_love_chacha aoikazto/sshd:ubuntu20.04
+
+docker run -d -p 8000:22 -v $(pwd):/workspaces -e PATH=/workspaces/components/gcc-linaro-6.3.1-2017.02-x86_64_aarch64-linux-gnu/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin -e LD_LIBRARY_PATH=./lib/ --name i_love_chacha aoikazto/sshd:ubuntu20.04
+
+docker exec -it i_love_chacha apt update -y && apt upgrade -y && apt install make ninja-build cmake
 
 wget https://releases.linaro.org/components/toolchain/binaries/6.3-2017.02/aarch64-linux-gnu/gcc-linaro-6.3.1-2017.02-x86_64_aarch64-linux-gnu.tar.xz -P components && \
 tar -zxvf components/gcc-linaro-6.3.1-2017.02-x86_64_aarch64-linux-gnu.tar.xz -C components
 
-wget https://github.com/Kitware/CMake/releases/download/v3.23.4/cmake-3.23.4-linux-x86_64.tar.gz -P components && \
-tar -zxvf components/cmake-3.23.4-linux-x86_64.tar.gz -C components
-
-rm -rf components/cmake-3.23.4-linux-x86_64.tar.gz && \
 rm -rf components/gcc-linaro-6.3.1-2017.02-x86_64_aarch64-linux-gnu.tar.xz
 
+docker exec i_love_chacha /bin/sh
